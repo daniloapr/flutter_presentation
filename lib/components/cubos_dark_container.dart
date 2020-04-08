225c7f45b8ae07d2cb84b6f9a6e1e2962f66dada
Danilo Rêgo <daniloapr@gmail.com>
2020-04-08 16:39:54 -0300

    finish: CubosDarkContainer

--- lib/components/cubos_dark_container.dart
+++ lib/components/cubos_dark_container.dart
@@ -8,35 +8,96 @@ class CubosDarkContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final height = context.size.height;
-    final width = context.size.width;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     //triangle size
-    final size = (height <= width) ? height / 2 : width / 2;
+    final size = (height <= width) ? height * 0.46 : width * 0.46;
 
     return Container(
       color: AppColors.black,
-      child: CustomPaint(
-        size: Size(size, size),
-        painter: _MyPainter(),
+      child: Stack(
+        children: <Widget>[
+          CustomPaint(
+            size: Size(size, size),
+            painter: _BlueTrianglePainter(),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: CustomPaint(
+              size: Size(size, size),
+              painter: _PinkTrianglePainter(),
+            ),
+          )
+        ],
       ),
     );
   }
 }
 
-class _MyPainter extends CustomPainter {
+class _BlueTrianglePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final path = Path()
+    final double _blueSize = size.height * (0.93); //height == width
+
+    final _whitePath = Path()
       ..moveTo(0, 0)
       ..lineTo(size.width, 0)
       ..lineTo(0, size.height)
       ..lineTo(0, 0);
 
-    final paint = Paint()
-      ..color = Colors.lightBlue
+    final _bluePath = Path()
+      ..moveTo(0, 0)
+      ..lineTo(_blueSize, 0)
+      ..lineTo(0, _blueSize)
+      ..lineTo(0, 0);
+
+    final _whitepaint = Paint()
+      ..color = Colors.white
       ..isAntiAlias = true;
 
-    canvas.drawPath(path, paint);
+    final _bluePaint = Paint()
+      ..color = AppColors.blue
+      ..isAntiAlias = true;
+
+    canvas.drawPath(_whitePath, _whitepaint);
+    canvas.drawPath(_bluePath, _bluePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter old) {
+    return false;
+  }
+}
+
+class _PinkTrianglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double _size = size.height;
+    final double _pinkSize = size.height * (1 - 0.93); //height == width
+
+    final _whitePath = Path()
+      ..moveTo(_size, _size)
+      ..lineTo(_size, 0)
+      ..lineTo(0, _size)
+      ..lineTo(_size, _size);
+
+    final _pinkPath = Path()
+      ..moveTo(_size, _size)
+      ..lineTo(_size, _pinkSize)
+      ..lineTo(_pinkSize, _size)
+      ..lineTo(_size, _size);
+
+    final _whitepaint = Paint()
+      ..color = Colors.white
+      ..isAntiAlias = true;
+
+    final _bluePaint = Paint()
+      ..color = AppColors.pink
+      ..isAntiAlias = true;
+
+    canvas.drawPath(_whitePath, _whitepaint);
+    canvas.drawPath(_pinkPath, _bluePaint);
   }
 
   @override
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,19 +1,10 @@
-import 'package:aula01/components/dark_container.dart';
-import 'package:aula01/components/text_link.dart';
-import 'package:aula01/components/topic_list.dart';
+import 'package:aula01/components/cubos_dark_container.dart';
 import 'package:flutter/material.dart';
 
 ///Introduction
 class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DarkContainer(
-      child: Center(
-        child: TopicList(
-          title: 'Conteúdo',
-          list: ['Tópico 1', 'Tópico 2'],
-        ),
-      ),
-    );
+    return CubosDarkContainer();
   }
 }
