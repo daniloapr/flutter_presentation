b6d02f5f1355400a8797d40d2340242b1776c5d2
Danilo Rêgo <daniloapr@gmail.com>
2020-04-08 12:39:10 -0300

    add: pink and blue colors

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const black = Color(0XFF353547);
+  static const pink = Color(0XFFff0178);
+  static const black = Color(0XFF353547);
 }
